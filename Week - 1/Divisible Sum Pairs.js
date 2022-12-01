6de1761c34db258bc6dad3a6e69a27523f86d190
VelascoCristian <81784222+VelascoCristian@users.noreply.github.com>
2022-11-30 21:32:34 -0300

    Correct Divisible Sum Pairs solution

--- Week - 1/Divisible Sum Pairs.js
+++ Week - 1/Divisible Sum Pairs.js
@@ -1,12 +1,13 @@
 function divisibleSumPairs(n, k, ar) {
-    let count = 0, position = 0;
-    while (ar.length > position) {
-        for (let i = 0; i < ar.length; i++) {
-            if ((ar[position] + ar[i]) % 3 === 0) {
-                if (ar[position] < ar[i]) count++;
+    let count = 0;
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < n; j++) {
+            if(i !== j && i < j) {
+                if((ar[i] + ar[j]) % k === 0) {
+                    count++;
+                }
             }
         }
-        position++;
     }
-    return count
+    return count;
 }
