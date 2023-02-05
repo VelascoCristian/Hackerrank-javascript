97a3afec37a097c21158a5306b7e6a47a6f16525
VelascoCristian <81784222+VelascoCristian@users.noreply.github.com>
2023-02-05 20:20:40 -0300

    Add Zig Zag Sequence solution

---
+++ Week - 3/Zig Zag Sequence.py
@@ -0,0 +1,20 @@
+# You have to change the programming language to be able to pass the exams,
+# at the moment of uploading the file, it is not possible to pass the exams with JavaScript.
+def findZigZagSequence(a, n):
+    a.sort()
+    mid = int(n / 2)
+    a[mid], a[n-1] = a[n-1], a[mid]
+
+    st = mid + 1
+    ed = n - 2
+    while(st <= ed):
+        a[st], a[ed] = a[ed], a[st]
+        st = st + 1
+        ed = ed - 1
+
+    for i in range (n):
+        if i == n-1:
+            print(a[i])
+        else:
+            print(a[i], end = ' ')
+    return
